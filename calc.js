2da12a97bbac41953476aeb8c22db3772242a137
anujaPatil14 <117199540+anujaPatil14@users.noreply.github.com>
2023-01-25 16:58:34 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,29 @@
+const display=document.querySelector('#display');
+const buttons=document.querySelectorAll('button');
+
+buttons.forEach((items) => {
+    items.onclick=() =>{
+        if(items.id=='clear'){
+            display.innerText='';
+        }else if(items.id=='backspace'){
+            let string=display.innerText.toString();
+            display.innerText=string.substr(0,string.length -1 );
+                }else if(display.innerText!='' && items.id=='equal'){
+                    display.innerText=eval(display.innerText);
+                } else if(display.innerText=='' && items.id=='equal'){
+                    display.innerText='Empty!';
+                    setTimeout(() => (display.innerText=''),2000); }
+                    else{
+                        display.innerText +=items.id
+                    }
+    }
+})
+const themeToggleBtn=document.querySelector('.theme-toggler');
+const calculator=document.querySelector('.calculator');
+const toggleIcon=document.querySelector('.toggler-icon');
+let isDark=true;
+themeToggleBtn.onclick=() =>{
+    calculator.classList.toggle('dark');
+    themeToggleBtn.classList.toggle('active');
+    isDark=!isDark;
+}
